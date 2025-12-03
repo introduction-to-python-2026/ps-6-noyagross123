8d1b98ae19f09abc9739bb4b488373a0ac5be372
noyagross123 <noyagross1@gmail.com>
2025-12-03 19:32:18 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,3 @@
-
 def create_codon_dict(file_path):
     codon_dict = {}
     with open(file_path, 'r') as file:
@@ -6,5 +5,3 @@ def create_codon_dict(file_path):
             codon, amino_acid, single_letter, full_name = line.split()
             codon_dict[codon] = single_letter
     return codon_dic
-
-
